692745943da9a81848007073c5978e20aa22e9e7
Ishan johari <ishanjohariij@gmail.com>
2026-01-01 22:50:00 +0530

    testing purpose only

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ const app = express();
 dotenv.config()
 
 console.log("Hello world")
+console.log("Hello this for testing purpose only kindly ignore")
 const port = process.env.PORT || 5001
 app.listen(5001,()=>{
     console.log(`Server is running on port: ${port}`)
