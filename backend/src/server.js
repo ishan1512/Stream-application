f612993725dc5d8f208b94fdfbac7f62a96e37e0
Ishan johari <ishanjohariij@gmail.com>
2026-01-01 22:46:39 +0530

    testing git

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv"
 const app = express();
 dotenv.config()
 
-
+console.log("Hello world")
 const port = process.env.PORT || 5001
 app.listen(5001,()=>{
     console.log(`Server is running on port: ${port}`)
