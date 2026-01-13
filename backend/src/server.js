f1537770bcf9c5558335ff99f63edb5998b04934
Ishan johari <ishanjohariij@gmail.com>
2026-01-13 15:02:03 +0530

    tanstack setup done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.routes.js';
@@ -11,6 +12,12 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true, //this allows frontend to send cookies
+  })
+);
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
