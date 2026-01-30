defe6f106d3b0d4437da3e400c9e681c6251615d
Ishan johari <ishanjohariij@gmail.com>
2026-01-30 13:19:05 +0530

    app completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import userRoutes from './routes/user.routes.js';
@@ -12,16 +13,27 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+
+const __dirname = path.resolve();
+
 app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true, //this allows frontend to send cookies
-  })
+  }),
 );
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 5001;
 app.listen(5001, () => {
